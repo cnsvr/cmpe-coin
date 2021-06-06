f28ddf4bfe664c39237cfd1dc1a7a4252daba5da
Berke.metin <berke.metin@boun.edu.tr>
2021-06-06 09:14:55 +0300

    Functions getBalanceOf(address), getAllTransactionsFor(address)

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,12 +1,32 @@
 import logging
 
 class CmpEBlockchain:
-    def __init__(self, chain, pendingTransactions = [], validationReward=1, difficulty=20):
-        self.chain = chain 
+    def __init__(self, chain, pendingTransactions=[], validationReward=1, difficulty=20):
+        self.chain = chain
         self.pendingTransactions = pendingTransactions
         self.validationReward = validationReward
         self.difficulty = difficulty
 
-
     def createInitialDummyBlock():
         pass
+
+    def getBalanceOf(self, address):
+        balance = 0
+
+        for transaction in self.getAllTransactionsFor(address):
+            if transaction.fromAddress == address:
+                balance -= transaction.amount
+            else:
+                balance += transaction.amount
+
+        return balance
+
+    def getAllTransactionsFor(self, address):
+        transactions = []
+
+        for block in self.chain:
+            for transaction in block.transactions:
+                if transaction.fromAddress == address or transaction.toAddress == address:
+                    transactions.append(transaction)
+
+        return transactions
