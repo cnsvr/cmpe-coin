b4ce544e7d4003ac08714832302c56308dfc9fdb
fileames <elifsemabalcioglu@gmail.com>
2021-06-09 20:48:17 +0300

    isChainValid implemented

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,5 +1,5 @@
 import logging
-import block
+from block import Block
 
 class CmpEBlockchain:
     def __init__(self, chain, pendingTransactions=[], validationReward=1, difficulty=20):
@@ -10,9 +10,44 @@ class CmpEBlockchain:
 
     def createInitialDummyBlock():
         pass
+
+    def isChainValid(self):
+        if len(self.chain) == 0:
+            logging.info("Chain is empty, must have genesis block.")
+            return False
+
+        lastBlock = self.chain[-1]
+        if not lastBlock.hasValidTransactions():
+            return False
+        if not lastBlock.calculateCurrBlockHash().startswith("0" * int(self.difficulty)):
+            return False
+
+        index = len(self.chain)-2
+
+        while index > 0:
+            prevBlock = self.chain[index]
+
+            if not prevBlock.hasValidTransactions():
+                return False
+            if not prevBlock.calculateCurrBlockHash().startswith("0" * int(self.difficulty)):
+                return False
+
+            prevBlockHash = prevBlock.calculateCurrBlockHash()
+
+            if prevBlockHash != lastBlock.prevBlockHash:
+                logging.info("Chain is not properly linked.")
+                return False
+
+            lastBlock = prevBlock
+
+        if len(self.chain[0].transactions) == 0 and self.chain[0].prevBlockHash == None:
+            logging.info("Genesis block is not correct.")
+            return True
+        
+        return False
     
     def addTransactionToPendingList(transx):
-        this.pendingTransactions.append(transx)
+        self.pendingTransactions.append(transx)
     
     def validatePendingTransactions(rewardAddress):
         pass
