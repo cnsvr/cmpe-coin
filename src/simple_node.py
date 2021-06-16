2e66c21fd119875a6ea78c736b3bf86c48454e70
Furkan Cansever <f.cansever67@gmail.com>
2021-06-16 13:26:54 +0300

    add doRandomInvalidTransactions function

--- src/simple_node.py
+++ src/simple_node.py
@@ -1,5 +1,6 @@
 from threading import Thread, Lock
 import pika
+import time
 import numpy as np
 import logging
 import json
@@ -31,7 +32,7 @@ class CmpECoinSimpleNode():
     def handleReceivedValidatedBlock(self):
         pass
     
-    # furkan
+
     def doRandomTransactions(self):
         # make a new thread and create random transaction and join thread to main thread.
         while True:
@@ -43,6 +44,20 @@ class CmpECoinSimpleNode():
             # create a new thread and call newTransaction function.
 
             newTransactionThread = Thread(target = self.newValidTransaction)
+            # newTransactionThread.start()
+            # newTransactionThread.join()
+    
+    def doRandomInvalidTransaction(self):
+        # make a new thread and create random transaction and join thread to main thread.
+        while True:
+            duration = np.random.exponential(self.meanTransactionInterDuration)
+            
+            # sleep for duration
+            sleep(duration)
+            
+            # create a new thread and call newTransaction function.
+
+            newTransactionThread = Thread(target = self.newInvalidTransaction)
             # newTransactionThread.start()
             # newTransactionThread.join()
 
@@ -72,6 +87,39 @@ class CmpECoinSimpleNode():
             return True
         else:
             return False
+
+    def newInvalidTransaction(self):
+        # Get the my current balance from blockchain.
+        self.blockChainMutex.acquire()
+        my_current_balance = self.blockChain.getBalance(self.myWallet.getPublicKey())
+        self.blockChainMutex.release()
+
+        self.myWallet.setCurrentBalance(my_current_balance)
+        amount = np.random.exponential(self.meanTransactionAmount)
+
+        # Find enough amount for valid transaction
+        while my_current_balance < amount:
+            amount = np.random.exponential(self.meanTransactionAmount)
+        
+        transaction = CmpETransaction(self.myWallet.getPublicKey(), pk_N, amount)
+
+        # Sign the transaction before sending it to the node.
+        if transaction.signTransaction(self.myWallet.getPrivateKey()):
+            # Transaction is valid
+            self.myWallet.setCurrentBalance(self.myWallet.getCurrentBalance() -  amount)
+
+            # Change amount and timestamp of transaction to make invalid.
+
+            transaction.amount = np.random.exponential(self.meanTransactionAmount) ** 2
+            transaction.timestamp = time.time()
+
+            # Convert to transaction to Json format
+            transaction_json=transaction.toJSON()
+            # send the json to node.
+            self.sendTransactionToDispatcher(transaction_json)
+            return True
+        else:
+            return False
     
     def sendTransactionToDispatcher(self, transaction_json):
         connection = pika.SelectConnection(self.parameters)        
@@ -80,9 +128,7 @@ class CmpECoinSimpleNode():
         channel.basic_publish(exchange='',
                       routing_key='transxRcvQ',
                       body=transaction_json)
-        
-    def doRandomInvalidTransaction(self):
-        pass
+
 
 
 '''
